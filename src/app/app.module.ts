fef1eb22b5a78e9993599879551d6b1717421c87
Christophe Fondeville <fondevillechristophe@gmail.com>
2022-12-21 21:30:24 +0100

    Add footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent } from './components/header/header.component';
 import {RouterOutlet} from "@angular/router";
 import { Error404Component } from './views/error404/error404.component';
 import { HomeComponent } from './views/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     Error404Component,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
     imports: [
         BrowserModule,
