82d82cebdef76f41a4fded2b46a38bbcfc7e4d6d
Christophe Fondeville <fondevillechristophe@gmail.com>
2022-12-21 23:02:58 +0100

    Configuration Header and Footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { Error404Component } from './views/error404/error404.component';
 import { HomeComponent } from './views/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [
@@ -17,11 +24,18 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     HomeComponent,
     FooterComponent
   ],
-    imports: [
-        BrowserModule,
-        RouterOutlet,
-        NgbModule
-    ],
+  imports: [
+    BrowserModule,
+    RouterOutlet,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
+import {MediaMatcher} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  mobileQuery: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  shouldRun = /(^|.)(stackblitz|webcontainer).(io|com)$/.test(window.location.host);
 }
