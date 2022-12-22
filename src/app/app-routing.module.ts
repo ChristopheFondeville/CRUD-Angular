54401bb6209f72b10845c7fec52592248eb5c92d
Christophe Fondeville <fondevillechristophe@gmail.com>
2022-12-22 09:26:13 +0100

    Add Routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule,Routes} from "@angular/router";
 import {HomeComponent} from "./views/home/home.component";
+import {CreateComponent} from "./views/create/create.component";
 import {Error404Component} from "./views/error404/error404.component";
-
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'create', component: CreateComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', component: Error404Component}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import {RouterOutlet} from "@angular/router";
-import { Error404Component } from './views/error404/error404.component';
-import { HomeComponent } from './views/home/home.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import {Error404Component} from './views/error404/error404.component';
+import {HomeComponent} from './views/home/home.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatMenuModule} from "@angular/material/menu";
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatListModule} from "@angular/material/list";
+import {CreateComponent} from './views/create/create.component';
+import {AppRoutingModule} from './app-routing.module';
+import {RouterOutlet} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -22,11 +24,11 @@ import {MatListModule} from "@angular/material/list";
     HeaderComponent,
     Error404Component,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
-    RouterOutlet,
     NgbModule,
     BrowserAnimationsModule,
     MatMenuModule,
@@ -34,9 +36,12 @@ import {MatListModule} from "@angular/material/list";
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    AppRoutingModule,
+    RouterOutlet,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
